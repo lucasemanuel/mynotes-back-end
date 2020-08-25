5005e857289d0d2131a1ab97b24fbf4d0915f21a
Lucas Emanuel <lucaxemanuel@gmail.com>
2020-08-25 17:21:24 -0300

    :construction: Adding routes api

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+Route::get('/users', 'Api\\UserController@index');
+Route::get('/user/{id}', 'Api\\UserController@show');
+
+Route::get('/notes', 'Api\\NoteController@index');
+Route::get('/note/{id}', 'Api\\NoteController@show');
+
