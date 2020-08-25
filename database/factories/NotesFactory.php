79705cd3ede5d9e209559dfbf3e95e148321d83c
Lucas Emanuel <lucaxemanuel@gmail.com>
2020-08-25 16:34:04 -0300

    :seedling: Adding Factories

--- database/factories/NotesFactory.php
+++ database/factories/NotesFactory.php
@@ -2,7 +2,7 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\User;
+use App\Note;
 use Faker\Generator as Faker;
 
 /*
@@ -16,10 +16,10 @@ use Faker\Generator as Faker;
 |
 */
 
-$factory->define(User::class, function (Faker $faker) {
+$factory->define(Note::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'title' => $faker->sentence,
+        'body' => $faker->paragraph,
+        'user_id' => $faker->randomDigit
     ];
 });
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserSeeder::class);
+        factory(\App\User::class, 9)->create();
+        factory(\App\Note::class, 30)->create();
     }
 }
