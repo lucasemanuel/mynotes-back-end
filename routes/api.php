b2fcc9951984387ef45e0c3c8ba25ff22e4b125a
Lucas Emanuel <lucaxemanuel@gmail.com>
2020-09-17 14:57:48 -0300

    :wrench: Access a routes user only with token

--- routes/api.php
+++ routes/api.php
@@ -14,12 +14,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//Route::middleware('auth:api')->get('/user', function (Request $request) {
-//    return $request->user();
-//});
+Route::group([
+    'prefix' => 'auth'
+], function ($router) {
+    Route::post('login', 'Api\\AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+});
 
-Route::get('/users', 'Api\\UserController@index');
-Route::get('/user/{id}', 'Api\\UserController@show');
+Route::group([
+    'prefix' => '/users',
+    'middleware' => 'apiJwt',
+], function ($router) {
+    Route::get('', 'Api\\UserController@index');
+    Route::get('/{id}', 'Api\\UserController@show');
+    Route::post('', 'Api\\UserController@store');
+    Route::post('{id}', 'Api\\UserController@update');
+    Route::delete('{id}', 'Api\\UserController@destroy');
+});
 
 Route::get('/notes', 'Api\\NoteController@index');
 Route::get('/note/{id}', 'Api\\NoteController@show');
