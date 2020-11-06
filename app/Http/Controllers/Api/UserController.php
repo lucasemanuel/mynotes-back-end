8dae15df4fdeab06cf89c03b8caa0894cdf382b7
Lucas Emanuel <lucaxemanuel@gmail.com>
2020-11-06 14:58:31 -0300

    :sparkles: Returning logged user

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -5,28 +5,15 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Users\StoreRequest;
 use App\User;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        $users = User::all();
-
-        return response($users);
+        return response(Auth::user());
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(StoreRequest $request)
     {
         $request->validated();
@@ -36,53 +23,5 @@ class UserController extends Controller
         $user->save();
 
         return response('', 201);
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        $user = User::where(['id' => $id])->firstOrFail();
-
-        return response($user);
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        $user = User::where(['id' => $id])->firstOrFail();
-
-        $request->validate([
-            'name' => 'bail|string|max:255|min:6',
-            'email' => 'unique:users|string|max:255',
-            'password' => 'confirmed|string|max:255|min:6',
-        ]);
-
-        $user->fill($request->all());
-        $user->save();
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        $user = User::where(['id' => $id])->firstOrFail();
-        $user->delete();
-
-        return response('', 204);
     }
 }
