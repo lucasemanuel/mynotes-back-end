9ef11fedffe86189dba3822a87cd19cff9cadd55
Lucas Emanuel <lucaxemanuel@gmail.com>
2020-11-09 09:51:23 -0300

    :recycle: Refactor auth

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -4,54 +4,37 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use JWTAuth;
 
 class AuthController extends Controller
 {
-    /**
-     * Get a JWT via given credentials.
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
     public function login(Request $request)
     {
         $credentials = $request->only(['email', 'password']);
 
-        if (!$token = auth('api')->attempt($credentials)) {
-            return response()->json(['error' => 'Unauthorized'], 401);
+        if (!$token = auth()->attempt($credentials)) {
+            return response(['message' => 'E-mail ou senha estão incorretos.'], 401);
         }
 
         return $this->respondWithToken($token);
     }
 
-    /**
-     * Get the authenticated User.
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public function me()
+    public function check()
     {
-        return response()->json(auth('api')->user());
+        try {
+            return response(['auth' => (Boolean) JWTAuth::parseToken()->authenticate()]);
+        } catch (\Exception $exception) {
+            return response(['auth' => false]);
+        }
     }
 
-    /**
-     * Log the user out (Invalidate the token).
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
     public function logout()
     {
-        auth('api')->logout();
+        auth()->logout();
 
-        return response()->json(['message' => 'Successfully logged out']);
+        return response(['message' => 'Até mais.']);
     }
 
-    /**
-     * Get the token array structure.
-     *
-     * @param  string $token
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
     protected function respondWithToken($token)
     {
         return response()->json([
--- routes/api.php
+++ routes/api.php
@@ -19,9 +19,8 @@ Route::group([
     'prefix' => 'auth'
 ], function ($router) {
     Route::post('login', 'Api\\AuthController@login');
-    Route::post('logout', 'AuthController@logout');
-    Route::post('refresh', 'AuthController@refresh');
-    Route::post('me', 'AuthController@me');
+    Route::post('check', 'Api\\AuthController@check');
+    Route::post('logout', 'Api\\AuthController@logout');
 });
 
 // Notes
