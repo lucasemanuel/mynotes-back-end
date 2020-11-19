28830bc4f257e4714c4fc0176748380392dd30d4
Lucas Emanuel <lucaxemanuel@gmail.com>
2020-11-19 18:21:11 -0300

    :seedling: Update seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,10 +12,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserSeeder::class);
-        factory(\App\User::class, 4)->create()->each(function ($user) {
-            $user->notes()->createMany(
-                factory(\App\Note::class, 3)->make()->toArray()
-            );
-        });
+        factory(\App\User::class)->create([
+            'email' => 'user@user.com',
+            'password' => '123456'
+        ]);
     }
 }
