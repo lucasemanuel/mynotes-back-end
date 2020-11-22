01042a646ae3643fc3f5a40d1e21b624ac859aa6
Lucas Emanuel <lucaxemanuel@gmail.com>
2020-11-22 13:27:32 -0300

    :sparkles: Create note

--- app/Http/Controllers/Api/NoteController.php
+++ app/Http/Controllers/Api/NoteController.php
@@ -3,16 +3,12 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Notes\StoreRequest;
 use App\Note;
 use Illuminate\Http\Request;
 
 class NoteController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         $notes = Note::all();
@@ -20,33 +16,17 @@ class NoteController extends Controller
         return response()->json($notes);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+    public function store(StoreRequest $request)
     {
-        //
+        $note = new Note();
+        $note->body = $request->body;
+        $note->is_favorite = (bool) $request->input('is_favorite', 0);
+        $note->user_id = auth()->user()->id;
+        $note->save();
+
+        return response($note->toArray(), 201);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
         $note = Note::with('user')->where(['id' => $id])->first();
@@ -54,35 +34,11 @@ class NoteController extends Controller
         return response()->json($note);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
         //
