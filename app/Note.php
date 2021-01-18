b7af0bab3307a834b9934428f42ed96e09f44cc4
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-01-18 11:30:09 -0300

    :sparkles: Casts note dates

--- app/Note.php
+++ app/Note.php
@@ -8,6 +8,11 @@ class Note extends Model
 {
     protected $table = 'notes';
 
+    protected $casts = [
+        'created_at' => 'datetime:d/m/Y H:i:s',
+        'updated_at' => 'datetime:d/m/Y H:i:s',
+    ];
+
     protected $fillable = [
         'body', 'is_favorite'
     ];
