cf312cde9265bff812df6e7b718b37ff1d87fa57
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-03-24 09:48:19 -0300

    remove comments

--- app/User.php
+++ app/User.php
@@ -11,20 +11,10 @@ class User extends Authenticatable implements JWTSubject
 {
     use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
     protected $fillable = [
         'name', 'email', 'password',
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
     protected $hidden = [
         'password',
     ];
