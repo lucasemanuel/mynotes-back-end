31bcdf31f2390030646e31fe6dd3d484a31cc99c
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-04-21 16:16:34 -0300

    set recovery password observer

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Observers\RecoveryPasswordObserver;
+use App\RecoveryPassword;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -27,8 +29,8 @@ class EventServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        parent::boot();
+        RecoveryPassword::observe(RecoveryPasswordObserver::class);
 
-        //
+        parent::boot();
     }
 }
