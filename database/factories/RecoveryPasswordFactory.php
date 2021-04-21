e6ff5f8c6ca6c53e959d411ed2009fe441ee8e6e
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-04-21 16:17:03 -0300

    add recovery password factory

---
+++ database/factories/RecoveryPasswordFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\User;
+use App\RecoveryPassword;
+use Carbon\Carbon;
+use Faker\Generator as Faker;
+
+$factory->define(RecoveryPassword::class, function (Faker $faker) {
+    return [
+        'user_id' => factory(User::class)
+    ];
+});
+
+$factory->state(RecoveryPassword::class, 'expired', [
+    'created_at' => Carbon::now()->subDay()
+]);
