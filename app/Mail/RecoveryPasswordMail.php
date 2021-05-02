6246a3d4d62e52fe1ace74c3c8ecb5d7b03d5d72
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-05-02 13:02:12 -0300

    add class mailable for recovery password

---
+++ app/Mail/RecoveryPasswordMail.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Mail;
+
+use App\RecoveryPassword;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class RecoveryPasswordMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    private $recoveryPassword;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(RecoveryPassword $recoveryPassword)
+    {
+        $this->recoveryPassword = $recoveryPassword;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        $email = $this->recoveryPassword->user->email;
+        $token = $this->recoveryPassword->token;
+
+        $this->to($email);
+        $this->subject('Recuperação de senha');
+
+        return $this->markdown('mail.recoveryPassword', [
+            'token' => $token
+        ]);
+    }
+}
