dd90646886dd1ef24cf6fc2291c9cf86db9db554
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-05-02 14:14:08 -0300

    add relationship with recovery passwords

--- app/User.php
+++ app/User.php
@@ -19,31 +19,26 @@ class User extends Authenticatable implements JWTSubject
         'password',
     ];
 
-    public function notes()
-    {
-        return $this->hasMany(Note::class);
-    }
-
     public function setPasswordAttribute($value)
     {
         $this->attributes['password'] = Hash::make($value);
     }
 
-    /**
-     * Get the identifier that will be stored in the subject claim of the JWT.
-     *
-     * @return mixed
-     */
+    public function notes()
+    {
+        return $this->hasMany(Note::class);
+    }
+
+    public function recoveryPasswords()
+    {
+        return $this->hasMany(RecoveryPassword::class);
+    }
+
     public function getJWTIdentifier()
     {
         return $this->getKey();
     }
 
-    /**
-     * Return a key value array, containing any custom claims to be added to the JWT.
-     *
-     * @return array
-     */
     public function getJWTCustomClaims()
     {
         return [];
